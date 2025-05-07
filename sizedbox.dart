04c5db9192f0d1187ba5fbde704b315434b669ac
Aliy08 <nabilaaliyyuandafortun@gmail.com>
2025-05-07 07:34:50 +0700

    SizedBox_NEW
    Ini code yang di upload

--- sizedbox.dart
+++ sizedbox.dart
@@ -1,18 +1,84 @@
 import 'package:flutter/material.dart';
 
 class SizedboxWidget extends StatelessWidget {
+
   const SizedboxWidget ({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        height: 40,
-        width: double.infinity,
-        child: ElevatedButton(
-          onPressed: () {}, 
-          child: Text("Simpan"),
+      backgroundColor: const Color.fromARGB(255, 200, 236, 252),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 30),
+          margin: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.05),
+                blurRadius: 12,
+                offset: const Offset(2, 4),
+              ),
+            ]
           ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  color: Colors.blue.shade50,
+                  shape: BoxShape.circle,
+                ),
+                child: const Icon(
+                  Icons.cloud_upload_outlined,
+                  size: 40,
+                  color: Colors.blueAccent,
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              Text(
+                "Upload Dokumen",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.grey.shade900,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                "Pastikan file yang di unggah berformat PDF atau DOCX.",
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey.shade600,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 24),
+
+              SizedBox(
+                height: 44,
+                width: double.infinity,
+                child: ElevatedButton.icon(
+                  onPressed: () {}, 
+                  icon: const Icon (Icons.upload_file_rounded, size: 20),
+                  label: const Text("Pilih File"),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueAccent,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    elevation: 2,
+                  ),
+                  ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
